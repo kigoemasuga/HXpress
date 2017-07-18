063ec85ba655933025307e9ddbdeed9c0fcc5316
kigoe <kigoemasuga@gmail.com>
2017-07-18 21:18:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/hxpress/WholeActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.hxpress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WholeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whole);
+    }
+}
