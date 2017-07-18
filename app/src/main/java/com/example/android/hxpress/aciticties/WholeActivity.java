031bf3181265e9585d271a6e92fe99d89bdc5880
kigoe <kigoemasuga@gmail.com>
2017-07-18 21:28:03 +0800

    修改了个bug

--- app/src/main/java/com/example/android/hxpress/aciticties/WholeActivity.java
+++ app/src/main/java/com/example/android/hxpress/aciticties/WholeActivity.java
@@ -1,13 +1,14 @@
-package com.example.android.hxpress;
+package com.example.android.hxpress.aciticties;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.android.hxpress.R;
 
 public class WholeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_whole);
+        setContentView(R.layout.activity_whole2);
     }
 }
